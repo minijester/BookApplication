0a7a16a976c00a4a1df0679a4af8375426afe604
Warut Sathorn <rt.digital1234@gmail.com>
2019-05-22 19:54:27 +0700

    Fix dependencies

--- app/src/main/java/com/haru/bookapp/api/BookManager.kt
+++ app/src/main/java/com/haru/bookapp/api/BookManager.kt
@@ -1,4 +1,14 @@
 package com.haru.bookapp.api
 
-class BookManager {
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object BookManager {
+    fun init(): BookService {
+        val retrofit = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://www.googleapis.com/books/v1/")
+            .build()
+        return retrofit.create(BookService::class.java)
+    }
 }
--- app/src/main/java/com/haru/bookapp/api/BookService.kt
+++ app/src/main/java/com/haru/bookapp/api/BookService.kt
@@ -1,4 +1,12 @@
 package com.haru.bookapp.api
 
+import com.haru.bookapp.model.Book
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface BookService {
+    @GET("volumes")
+    fun getBook() : Call<List<Book>>
+
 }
